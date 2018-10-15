8466f35755a9016360c2a4d1af9baaef6f06d273
BIFFI <Yuichirobiffi@icloud.com>
2018-10-15 21:39:49 +0900

    add new file

---
+++ Chara_RaceGame/Assets/Floor_gene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor_gene : MonoBehaviour {
+
+    public GameObject FloorPrefab;
+
+    private float i, j;
+
+	// Use this for initialization
+	void Start () {
+
+        for (i=0.0f;i<300.0f;i++)
+        {
+            for (j=0.0f;j<3.0f;j++)
+            {
+                GameObject Floor = Instantiate(FloorPrefab) as GameObject;
+
+                Floor.transform.position = new Vector3(0.0f+j,0.0f,0.0f+i);
+            }
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
