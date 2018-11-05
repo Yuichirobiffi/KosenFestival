9449cd6fa6f3d5e1c80ff4e0bcafd3b8b9fe4429
BIFFI <Yuichirobiffi@icloud.com>
2018-11-06 00:39:33 +0900

    コントローラ追加予定

---
+++ Chara_RaceGame/Assets/Scripts/DuplicateDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuplicateDestroy : MonoBehaviour {
+    //なんかうまくいかなくなった
+    //private void OnCollisionEnter(Collision other){
+        //かぶったら床系(速度上昇、速度減少)削除する
+        //if (other.gameObject.tag == "wall"){
+            //Debug.Log("D");
+            //Destroy(gameObject);
+        //}
+    //}
+    private void OnTriggerEnter(Collider other){
+        //かぶったら床系(速度上昇、速度減少)削除する
+        if (other.gameObject.tag == "wall"){
+            //Debug.Log("D");
+            Destroy(gameObject);
+        }
+    }
+}
